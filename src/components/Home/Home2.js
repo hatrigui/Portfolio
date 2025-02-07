1fd43763da227c93fca2776ee581dd994e11d1bf
hatrigui <contact.trigui.hatem@gmail.com>
2025-02-08 00:16:55 +0100

    done with home page

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -14,43 +14,43 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              LET ME <span className="blue"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
+            <b className="blue"> AI Engineer </b> in the making ⌛
+              <br />
+              <br />
               I fell in love with programming and I have at least learnt
               something, I think… 🤷‍♂️
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> Python, Matlab, C, C++, Javascript. </b>
+                <b className="blue"> Python, Matlab, C, C++, Java, Javascript. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              My field of interest lies in developing innovative solutions through AI, exploring its applications across various domains.  
+<i>
+  I am particularly passionate about 
+  <b className="blue"> computer vision and segmentation in healthcare</b>, 
+  as well as 
+  <b className="blue"> object detection in industrial settings</b>.
+</i>
+<br />
+<br />
+Whenever possible, I apply my expertise in AI-driven technologies, utilizing  
+<b className="blue"> deep learning</b> and  
+<i>
+  <b className="blue"> advanced machine learning frameworks </b>
+</i> 
+to build intelligent and efficient solutions.
+<br />
+<br />
+My goal is to continuously explore and expand AI’s potential to solve real-world challenges, creating impactful innovations across multiple industries 🚀.
+
             </p>
           </Col>
-          <Col md={4} className="myAvtar">
+          <Col md={4} className="myAvtar mt-5">
             <Tilt>
               <img src={myImg} className="img-fluid" alt="avatar" />
             </Tilt>
